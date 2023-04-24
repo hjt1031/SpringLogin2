3d6d6f35fa3a843e045bc34af81181f181eac233
juntae ha <hjt1031@naver.com>
2023-04-24 21:08:12 +0900

    홈화면 추가

---
+++ src/main/java/hello/login/domain/login/LoginService.java
@@ -0,0 +1,12 @@
+package hello.login.domain.login;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LoginService {
+
+
+}
+
