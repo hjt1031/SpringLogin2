683728fc1cc65c1be1295ef7c308e620dae73730
juntae ha <hjt1031@naver.com>
2023-05-02 22:08:08 +0900

    세션 정보와 타임아웃 설정

---
+++ src/main/java/hello/login/web/session/SessionInfoController.java
@@ -0,0 +1,36 @@
+package hello.login.web.session;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.Date;
+
+@Slf4j
+@RestController
+public class SessionInfoController {
+
+    @GetMapping("/session-info")
+    public String sessionInfo(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        if(session == null) {
+            return "세션이 없습니다.";
+        }
+
+
+        //세션 데이터 출력
+        session.getAttributeNames().asIterator()
+                .forEachRemaining(name -> log.info("session name={}, value={}", name, session.getAttribute(name)));
+
+        log.info("sessionId={}", session.getId());
+        log.info("maxInactiveInterval={}", session.getMaxInactiveInterval()); //세션 유효시간  
+        log.info("creationTime={}", new Date(session.getCreationTime())); //세션 생성일시
+        log.info("lastAccessedTime={}", new Date(session.getLastAccessedTime())); //세션과 연결된 사용자가 최근에 서버에 접근한 시간, 클라이언트에서 서버로
+        log.info("isNew={}", session.isNew());// 새로 생성된 세션인지, 아니면 이미과게어 만들어졌고, 클라이언트에서 서버로 sessionId 를 요청해서 조회된 세션인지 여부
+
+        return "세션 출력";
+    }
+}
